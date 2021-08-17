95f06079535aa144f636717f61621f5e433ff97d
HubbeKing <github@hubbe.club>
2021-08-17 12:49:49 +0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+REPO=registry.hubbe.club/autoapply
+TAG=$(git describe --tags --abbrev=0)
+
+buildah bud --tag $REPO:$TAG --tag $REPO:latest .
+buildah push $REPO:$TAG
+buildah push $REPO:latest .
